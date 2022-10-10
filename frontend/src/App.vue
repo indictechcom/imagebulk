4d5f0ea2687d11d25f3a0417b54a076fce9152dd
DanielGraham123 <dg.danielboaz@yahoo.com>
2022-10-10 23:01:56 +0100

    moved frontend to separate vue component and implemented internationalization

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <div class="text-h5">{{ $t('nav-name') }}</div>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <ImageBulkComponent />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import ImageBulkComponent from "./components/ImageBulkComponent.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    ImageBulkComponent,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
